00b2821102c0e984f56c95178024a5e5858af1e6
peterkrumb <petekrumb@gmail.com>
2020-04-16 00:56:56 -0500

    deploy

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ fluke: "swag" });
+  res.send({ hi: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
